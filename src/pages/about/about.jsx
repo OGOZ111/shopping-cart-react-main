c17f915ba52c0b48598e18194f3ff18260dfca58
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-11-30 12:28:23 +0200

    add category cards and pics

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,95 @@
+import { Link } from "react-router-dom";
 import React from "react";
+import "./about.css";
+import category1 from "../../assets/categories/kitchen.jpg"
+import category2 from "../../assets/categories/garden.jpg"
+import category3 from "../../assets/categories/pets.jpg"
+import category4 from "../../assets/categories/clothing.webp"
+import category5 from "../../assets/categories/kids.jpg"
+import category6 from "../../assets/categories/office.jpg"
+import category7 from "../../assets/categories/car.jpg"
+import category8 from "../../assets/categories/seasonal.webp"
+import category9 from "../../assets/categories/other.png"
+
+
+
+
+
+
+
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+  <>
+
+
+
+  <h1>About</h1>
+
+<div className="contain">
+
+ <div className="categorybox"> 
+ <div><img className="img2" src={category1} /></div>
+ <div className="catnme"> Kitchen </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category2} /></div>
+ <div className="catnme"> Garden </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category3} /></div>
+ <div className="catnme"> Pets </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category4} /></div>
+ <div className="catnme"> Clothing </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category5} /></div>
+ <div className="catnme"> Kids </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category6} /></div>
+ <div className="catnme"> Office </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category7} /></div>
+ <div className="catnme"> Car </div>
+ <button> Show more</button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category8} /></div>
+ <div className="catnme"> Seasonal </div>
+ <button> Show more </button>
+</div>
+
+<div className="categorybox"> 
+ <div><img className="img2" src={category9} /></div>
+ <div className="catnme"> Other </div>
+ <button> Show more </button>
+</div>
+
+</div>
+
+  
+  
+  
+  
+  </>
+  
+  
+  );
 };
